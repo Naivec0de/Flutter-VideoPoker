8db8cf78fba68d307c89161ace3127507f5d49bf
sky-48 <41387880+sky-48@users.noreply.github.com>
2018-12-26 16:13:27 -0800

    Add Payout Table

--- lib/main.dart
+++ lib/main.dart
@@ -7,105 +7,128 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Video Poker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class PayoutTable extends StatefulWidget {
+  @override
+  State createState() => PayoutTableState();
+}
+
+class PayoutTableState extends State<PayoutTable> {
+  int _highlight = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    const names = [
+      "Royal Flush",
+      "Straight Flush",
+      "Four of a Kind",
+      "Full House",
+      "Flush",
+      "Straight",
+      "Three of a Kind",
+      "Two Pair",
+      "Jacks or Better",
+    ];
+    const basePayRate = [250, 50, 25, 9, 6, 4, 3, 2, 1];
+    const specialRate = [4000, 250, 125, 45, 30, 20, 15, 10, 5];
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 2, right: 2),
+      child: FittedBox(
+        fit: BoxFit.fitWidth,
+        child: Container(
+          decoration: BoxDecoration(color: Colors.yellow),
+          padding: EdgeInsets.only(left: 1, right: 1, top: 2, bottom: 2),
+          child: Row(children: [
+            _buildColumn(0, names.map((name) => name.toUpperCase()).toList(),
+                width: 140, alignment: Alignment.centerLeft),
+            _buildColumn(1, basePayRate.map((rate) => '${rate * 1}').toList()),
+            _buildColumn(2, basePayRate.map((rate) => '${rate * 2}').toList()),
+            _buildColumn(3, basePayRate.map((rate) => '${rate * 3}').toList()),
+            _buildColumn(4, basePayRate.map((rate) => '${rate * 4}').toList()),
+            _buildColumn(5, specialRate.map((rate) => '${rate * 1}').toList()),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColumn(int id, List<String> content,
+      {double width = 40, alignment = Alignment.centerRight}) {
+    return GestureDetector(
+      onTap: () {
+        if (id != 0) {
+          setState(() {
+            _highlight = id;
+          });
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(color: Colors.yellow),
+        child: Column(
+          children: content.map((text) {
+            return Container(
+              alignment: alignment,
+              color: id == _highlight ? Colors.red : Colors.grey,
+              height: 20,
+              width: width,
+              margin: EdgeInsets.only(left: 1, right: 1),
+              child: Text(
+                text,
+                style: TextStyle(color: Colors.yellow),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Flutter Video Poker"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        color: Colors.blueAccent,
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
           // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              flex: 2,
+              child: PayoutTable(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Expanded(
+              flex: 3,
+              child: Text("Play Area"),
             ),
+            Expanded(
+              flex: 1,
+              child: Text("Buttons"),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
